d7fca7fb5f50aca84881541a2a1d43a202cecfa6
Iroh-Omolola <irohomolola@gmail.com>
2023-04-07 19:47:59 +0100

    made changed to interface

--- services/slices/chartSlice.ts
+++ services/slices/chartSlice.ts
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+interface IProps{
+  top_locations: [],
+  graph_data:[],
+  top_sources:[],
+}
 export const dataApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://fe-task-api.mainstack.io" }),
   endpoints: (builder) => ({
-    getData: builder.query<void, string>({
+    getData: builder.query<IProps, string>({
       query: (date) => `/${date}`,
     }),
   }),
